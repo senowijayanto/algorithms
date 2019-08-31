7ba4134cfc12f38208a20c1d3aa2dd19c6b963c7
Seno Wijayanto <senowijayanto@gmail.com>
2019-08-31 21:14:48 +0700

    Give it comment into reverse algorithm

--- reversestring/main.go
+++ reversestring/main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
+
+	// --- Directions
+	// Given a string, return a new string with the reversed
+	// order of characters
+	// --- Examples
+	// 	reverse('apple') = 'leppa'
+	// 	reverse('hello') = 'olleh'
+	// 	reverse('Greetings!') = '!sgniteerG'
+
 	fmt.Println("Reverse `hello`: ", reverse("hello"))
 }
 
